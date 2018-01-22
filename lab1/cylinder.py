ed3fa5d79ad8529e3934820ec7a51d8542d5d25f
Brad <anderbra@oregonstate.edu>
2018-01-22 13:35:42 -0800

    New repo with old folders

---
+++ lab1/cylinder.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from math import pi
+
+height = 5
+radius = 3
+
+volume = pi*(radius**2) * height
+
+print 'The volume of the cylinder is {}'.format(volume)
---
+++ lab1/torus.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from math import pi
+
+
+iD = 4
+oD = 3
+
+volume = 2 * pi**2 * oD * iD**2
+
+print 'The volume of the torus is {}'.format(volume)
